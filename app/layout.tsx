28c754de47c300e7cc3f1c5bd8cd10251b3f7d03
TrapGodBjornn <bshurd42@gmail.com>
2025-12-01 01:53:29 -0500

    Google Analytics implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import StyledComponentsRegistry from './lib/registry';
 
@@ -20,6 +21,19 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FKM5KFGDBB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FKM5KFGDBB');
+          `}
+        </Script>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
